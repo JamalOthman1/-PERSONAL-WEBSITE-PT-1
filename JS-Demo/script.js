c1b7caf4d59b49ca9d1ba165341cda78505701de
mustafa Jabr <jabrmustafa@gmail.com>
2024-01-23 10:14:15 -0500

    block09

---
+++ JS-Demo/script.js
@@ -0,0 +1,37 @@
+// command()
+alert()
+
+// English: Fetch the ball!
+// javascript: fetch(the ball)
+
+// command(further details)
+//alert("exact text")
+
+//no qoutes tp computr something
+//alert(3 +6)
+
+
+
+console.log("exact text")
+console.log(3 +6) 
+document.write(text)
+console.log('hello'+'world') 
+let x
+x=5
+x=6
+const z =3 
+z=3
+ 
+let username = "Bob"
+let userage =5.5
+let currentDate = '18 Jan 2024'
+console.log('hello'+ username)
+console.log(x+z)
+ let itsrainingrightnow = false
+typeof 10
+let iamusinganumbrella=
+ 
+
+
+
+
---
+++ block07/script.js
@@ -0,0 +1,40 @@
+console.log('introMessage')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const intromessage = 'you have recived this message becuse you have'
+
+console.log(intromessage)
+
+/*3. make 3 varibables
+   -1st variable must equal 10
+   -2nd var must equal 40
+   -3rd var must equal 39
+*/
+
+const num1 = 2* 5
+const num2 =8 + (8 *4)
+const num3 =289 %50 
+
+// const isEven
+
+console.log('number 1:',num1)
+console.log('number 2:', num2)
+console.log('number 3:' num3)
+// 4.(comment code)
+
+// 5.creat a dialogue box with the values
+alert(intromessage + '\n' + num1 + '-' + num2 + '-' + num3)
